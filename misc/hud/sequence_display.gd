ad95e6d3d11dc4729baf37a4f7552617cacc895b
Lumi <catstar27@tutanota.com>
2025-01-10 09:39:58 -0500

    Started work on new initiative display

--- misc/hud/sequence_display.gd
+++ misc/hud/sequence_display.gd
@@ -1,4 +1,4 @@
-extends VBoxContainer
+extends Control
 class_name SequenceDisplay
 
 func _ready()->void:
@@ -6,18 +6,5 @@ func _ready()->void:
 	EventBus.subscribe("TURN_ENDED", self, "hide_top")
 	EventBus.subscribe("COMBAT_ENDED", self, "hide")
 
-func update_display(order: Array[Character])->void:
-	for child in get_children():
-		child.queue_free()
-	for character in order:
-		var panel: PanelContainer = PanelContainer.new()
-		panel.use_parent_material = true
-		var new_label: Label = Label.new()
-		new_label.use_parent_material = true
-		new_label.text = character.display_name
-		add_child(panel)
-		panel.add_child(new_label)
+func update_display(_order: Array[Character])->void:
 	show()
-
-func hide_top()->void:
-	get_child(0).queue_free()
