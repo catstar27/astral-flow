cc9630413088581f1665f851dfab05a49ebc9d81
Elijah Ingerson <catstar27@tutanota.com>
2025-07-27 02:03:03 -0700

    Assets

--- gui/skill_menu/skill_menu.gd
+++ gui/skill_menu/skill_menu.gd
@@ -1,6 +1,6 @@
 extends MarginContainer
 class_name SkillMenu
-## Menu showing all skill trees and their associated skills
+## Menu showing the skill tree triangle
 
 var character: Character ## Character to show skills for
 signal opened ## Emitted when this menu is opened
